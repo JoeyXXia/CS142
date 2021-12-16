f128992c0a268b53ac7f97cd91b5d604213a5ff3
xu <270197253@qq.com>
2021-12-16 17:05:36 +0800

    scroll event

--- projects/project5/project5react/components/topBar/TopBar.jsx
+++ projects/project5/project5react/components/topBar/TopBar.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
-import {
-  AppBar, Toolbar, Typography
-} from '@material-ui/core';
-import './TopBar.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-/**
- * Define TopBar, a React componment of CS142 project #5
- */
-class TopBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-  render() {
-    return (
-      <AppBar className="cs142-topbar-appBar" position="absolute">
+export default function TopBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
         <Toolbar>
-          <Typography variant="h5" color="inherit">
-              This is the TopBar component
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
           </Typography>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-    );
-  }
+    </div>
+  );
 }
-
-export default TopBar;
--- projects/project5/project5react/components/userList/userList.jsx
+++ projects/project5/project5react/components/userList/userList.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import {
   Divider,
   List,
   ListItem,
   ListItemText,
   Typography,
-}
-from '@material-ui/core';
-import './userList.css';
+} from "@material-ui/core";
+import "./userList.css";
+import { Link } from "react-router-dom";
 
 /**
  * Define UserList, a React componment of CS142 project #5
@@ -21,8 +21,9 @@ class UserList extends React.Component {
     return (
       <div>
         <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://material-ui.com/demos/lists/">Lists</a> and <a href="https://material-ui.com/demos/dividers">Dividers</a> to
+          This is the user list, which takes up 3/12 of the window. You might
+          choose to use <a href="https://material-ui.com/demos/lists/">Lists</a>{" "}
+          and <a href="https://material-ui.com/demos/dividers">Dividers</a> to
           display your users like so:
         </Typography>
         <List component="nav">
